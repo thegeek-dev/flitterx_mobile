33501f4c555516e0ddf89baf5e0b2ac70f830e58
Neeraj Patel <njp2804@gmail.com>
2022-03-30 16:29:05 +0530

    For Linux Sync

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,16 @@ import 'package:flitterx_mobile/ui/landing_page.dart';
 import 'package:flitterx_mobile/ui/tournament_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.black, // navigation bar color
+    statusBarColor: Color.fromRGBO(0, 0, 20, 1), // status bar color
+  ));
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/ui/tournament_page.dart
+++ lib/ui/tournament_page.dart
@@ -1,6 +1,8 @@
 // @dart=2.9
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flitterx_mobile/components/detail_card.dart';
 import 'package:flitterx_mobile/ui/reg_form.dart';
 import 'package:flutter/material.dart';
@@ -22,201 +24,219 @@ class _TournamentPageState extends State<TournamentPage> {
             "\n\n" +
         bullet +
         " Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam in ligula at orci tincidunt lacinia posuere in elit. Aliquam posuere venenatis facilisis. Praesent aliquet est mi, blandit auctor nunc tristique eget. ";
-
+    Color backgroundClr = const Color.fromRGBO(0, 0, 20, 1);
     Color accent = const Color.fromRGBO(0, 0, 75, 1);
     Color bgColor = const Color.fromRGBO(0, 0, 20, 1);
     Size size = MediaQuery.of(context).size;
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
-        backgroundColor: accent,
+        centerTitle: true,
+        backgroundColor: backgroundClr,
         title: Text("Tournament"),
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
         color: bgColor,
         child: SingleChildScrollView(
-            physics: const BouncingScrollPhysics(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: CachedNetworkImage(
-                      width: size.width / 1.1,
-                      height: size.height / 3,
-                      imageUrl:
-                          "https://i.ibb.co/VBhrpR0/81b-Rac-T8el-L-SL1500.jpg",
-                      placeholder: (context, url) =>
-                          CircularProgressIndicator(),
-                      errorWidget: (context, url, error) => Icon(Icons.error),
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                Center(
-                  child: Text(
-                    "BGMI Tournament",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: size.width / 14,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 5),
-                    child: Text("14th March, College, Jaipur",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: size.width / 25,
-                            fontWeight: FontWeight.w100)),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    DetailCard(
-                      title: "Time",
-                      data: "5:00 PM",
-                    ),
-                    DetailCard(
-                      title: "Entry Fee",
-                      data: "Rs. 99",
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    DetailCard(
-                      title: "Check-in Time",
-                      data: "15 min before start",
-                    ),
-                    DetailCard(
-                      title: "Format",
-                      data: "Battle Royale",
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: size.height / 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    DetailCard(
-                      title: "Team Size",
-                      data: "1 Person",
-                    ),
-                    DetailCard(
-                      title: "Prizes",
-                      data: "Rs. 5000",
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: size.height / 25),
-                  child: Center(
-                    child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const RegForm()),
-                          );
-                        },
-                        style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.blue),
-                          overlayColor:
-                              MaterialStateProperty.all(Colors.white54),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          ),
-                        ),
-                        child: Container(
-                          width: size.width / 2,
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Register",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: size.width / 16,
-                                    fontWeight: FontWeight.w700),
-                              ),
-                            ),
-                          ),
-                        )),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(size.height / 30),
-                  child: Text(
-                    "Instructions :",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: size.width / 19,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    size.height / 30,
-                    0,
-                    size.height / 30,
-                    size.height / 30,
-                  ),
-                  child: Text(filler,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: size.width / 25,
-                          fontWeight: FontWeight.w100)),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(size.height / 30),
-                  child: Text(
-                    "Rules :",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: size.width / 19,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(
-                    size.height / 30,
-                    0,
-                    size.height / 30,
-                    size.height / 30,
-                  ),
-                  child: Text(filler,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: size.width / 25,
-                          fontWeight: FontWeight.w100)),
-                )
-              ],
-            )),
+          physics: const BouncingScrollPhysics(),
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("CurrentTournaments")
+                .snapshots(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Container(color: Colors.white, child: Text('aywgud'));
+            },
+          ),
+        ),
       ),
     ));
   }
 }
+
+
+// child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 SizedBox(
+//                   height: size.height / 30,
+//                 ),
+//                 Center(
+//                   child: ClipRRect(
+//                     borderRadius: BorderRadius.circular(15),
+//                     child: CachedNetworkImage(
+//                       width: size.width / 1.1,
+//                       height: size.height / 3,
+//                       imageUrl:
+//                           "https://i.ibb.co/VBhrpR0/81b-Rac-T8el-L-SL1500.jpg",
+//                       placeholder: (context, url) =>
+//                           CircularProgressIndicator(),
+//                       errorWidget: (context, url, error) => Icon(Icons.error),
+//                       fit: BoxFit.fitHeight,
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   height: size.height / 30,
+//                 ),
+//                 Center(
+//                   child: Text(
+//                     "BGMI Tournament",
+//                     style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: size.width / 14,
+//                         fontWeight: FontWeight.w700),
+//                   ),
+//                 ),
+//                 Center(
+//                   child: Padding(
+//                     padding: const EdgeInsets.only(top: 5),
+//                     child: Text("14th March, College, Jaipur",
+//                         style: TextStyle(
+//                             color: Colors.white,
+//                             fontSize: size.width / 25,
+//                             fontWeight: FontWeight.w100)),
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   height: size.height / 30,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     DetailCard(
+//                       title: "Time",
+//                       data: "5:00 PM",
+//                     ),
+//                     DetailCard(
+//                       title: "Entry Fee",
+//                       data: "Rs. 99",
+//                     ),
+//                   ],
+//                 ),
+//                 SizedBox(
+//                   height: size.height / 30,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     DetailCard(
+//                       title: "Check-in Time",
+//                       data: "15 min before start",
+//                     ),
+//                     DetailCard(
+//                       title: "Format",
+//                       data: "Battle Royale",
+//                     ),
+//                   ],
+//                 ),
+//                 SizedBox(
+//                   height: size.height / 30,
+//                 ),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                   children: [
+//                     DetailCard(
+//                       title: "Team Size",
+//                       data: "1 Person",
+//                     ),
+//                     DetailCard(
+//                       title: "Prizes",
+//                       data: "Rs. 5000",
+//                     ),
+//                   ],
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.only(top: size.height / 25),
+//                   child: Center(
+//                     child: TextButton(
+//                         onPressed: () {
+//                           Navigator.push(
+//                             context,
+//                             MaterialPageRoute(
+//                                 builder: (context) => const RegForm()),
+//                           );
+//                         },
+//                         style: ButtonStyle(
+//                           backgroundColor:
+//                               MaterialStateProperty.all(Colors.blue),
+//                           overlayColor:
+//                               MaterialStateProperty.all(Colors.white54),
+//                           shape:
+//                               MaterialStateProperty.all<RoundedRectangleBorder>(
+//                             RoundedRectangleBorder(
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                           ),
+//                         ),
+//                         child: Container(
+//                           width: size.width / 2,
+//                           child: Center(
+//                             child: Padding(
+//                               padding: const EdgeInsets.all(8.0),
+//                               child: Text(
+//                                 "Register",
+//                                 style: TextStyle(
+//                                     color: Colors.white,
+//                                     fontSize: size.width / 16,
+//                                     fontWeight: FontWeight.w700),
+//                               ),
+//                             ),
+//                           ),
+//                         )),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.all(size.height / 30),
+//                   child: Text(
+//                     "Instructions :",
+//                     style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: size.width / 19,
+//                         fontWeight: FontWeight.w700),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.fromLTRB(
+//                     size.height / 30,
+//                     0,
+//                     size.height / 30,
+//                     size.height / 30,
+//                   ),
+//                   child: Text(filler,
+//                       style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: size.width / 25,
+//                           fontWeight: FontWeight.w100)),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.all(size.height / 30),
+//                   child: Text(
+//                     "Rules :",
+//                     style: TextStyle(
+//                         color: Colors.white,
+//                         fontSize: size.width / 19,
+//                         fontWeight: FontWeight.w700),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.fromLTRB(
+//                     size.height / 30,
+//                     0,
+//                     size.height / 30,
+//                     size.height / 30,
+//                   ),
+//                   child: Text(filler,
+//                       style: TextStyle(
+//                           color: Colors.white,
+//                           fontSize: size.width / 25,
+//                           fontWeight: FontWeight.w100)),
+//                 )
+//               ],
+//             )
